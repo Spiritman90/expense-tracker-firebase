4145cba79456f88efb5248b1844966c32f34afa1
spiritman90 <phemmyadey@gmail.com>
2022-04-12 12:58:18 +0100

    added authentication

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -15,7 +15,8 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 //initialize service
-const projectFirestore = firebase.fireStore();
+const projectFirestore = firebase.firestore();
 const projectAuth = firebase.auth();
+const timestamp = firebase.firestore.Timestamp;
 
-export { projectFirestore, projectAuth };
+export { projectFirestore, projectAuth, timestamp };
